7cb3c7e19e299bf00c3e9fb8a394b54bca568e67
Karl Beecken <karl.beecken@icloud.com>
2019-05-02 20:27:26 +0200

    (hopefully) fix heroku timeout

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const DelayedResponse = require('http-delayed-response')
 const { crunchDate, crunchListAll, crunchList } = require('../scrape')
 const { getLocations } = require('../map')
 
@@ -17,7 +18,11 @@ router.get('/scrape/list/all', function(req, res, next) {
 })
 
 router.get('/scrape/list/map', function(req, res, next) {
-  getLocations().then((list) => res.json({ generated: Date.now(), list }))
+  let delayed = new DelayedResponse(req, res, next)
+  delayed.json()
+  delayed.wait(100000)
+  let promise = getLocations()
+  delayed.end(promise, promise)
 })
 
 module.exports = router;
