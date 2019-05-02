f6313f1a640b0a4540509b78a02057160a3cb7d5
Karl Beecken <karl.beecken@icloud.com>
2019-05-02 20:27:26 +0200

    Revert "(hopefully) fix heroku timeout"
    This reverts commit 7cb3c7e19e299bf00c3e9fb8a394b54bca568e67.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const DelayedResponse = require('http-delayed-response')
 const { crunchDate, crunchListAll, crunchList } = require('../scrape')
 const { getLocations } = require('../map')
 
@@ -18,11 +17,7 @@ router.get('/scrape/list/all', function(req, res, next) {
 })
 
 router.get('/scrape/list/map', function(req, res, next) {
-  let delayed = new DelayedResponse(req, res, next)
-  delayed.json()
-  delayed.wait(100000)
-  let promise = getLocations()
-  delayed.end(promise, promise)
+  getLocations().then((list) => res.json({ generated: Date.now(), list }))
 })
 
 module.exports = router;
