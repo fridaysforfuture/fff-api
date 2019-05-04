2174f3cc9fd5de8c04ed4c68ae2035701b2bde18
Karl Beecken <karl.beecken@icloud.com>
2019-05-04 17:18:53 +0200

    add mapdata for the text generator

--- map.js
+++ map.js
@@ -8,6 +8,7 @@ const { crunchDate, crunchListAll, crunchList, crunchListSecond } = require('./s
 
 const file = 'public/mapdata.js'
 const file2 = 'public/mapdata2.js'
+const file_textgen = 'public/mapdata_textgen.js'
 
 function deUmlaut (value) {
   value = value.toLowerCase()
@@ -104,6 +105,37 @@ async function getLocationsSecond () { // generates the Leaflet data from the se
   return locations
 }
 
+async function getLocationsTextgen () { // generates the Leaflet data from the first list on the pages, for textgen use
+  let locations = []
+  const list = await crunchList().then(data => {
+    return data
+  })
+  for (const item of list) {
+    const coords = await getCityCoords(item.city, item.time, item.place).then(res => {
+      if (res.city !== undefined) return res
+    })
+    locations.push(coords)
+  }
+  console.log('Total entries (textgen): ' + locations.length)
+  let markers = ''
+  locations.forEach(val => {
+    markers += `L.marker([${val.lat},${val.lon}]).addTo(map).bindPopup('<b>${val.city}</b></br>${val.time}<br>${val.place}<br><button class="btn btn-outline-primary">Ort w&auml;hlen</button>');
+`
+  })
+  fs.unlink(file_textgen, (err) => {
+    if (err) {
+      console.log("failed to delete textgen mapdata: " + err)
+    } else {
+      console.log('successfully deleted textgen mapdata')
+    }
+  })
+  fs.writeFile(file_textgen, markers, err => {
+    if (err) return console.error(err)
+    console.log('Textgen file successful saved!')
+  })
+  return locations
+}
+
 // console.time()
 // getCityCoords('Berlin').then(data => {
 //   console.log(data)
@@ -185,6 +217,13 @@ setTimeout(() => { // prevent f4f server from blocking the request
   console.time('loc2')
   getLocationsSecond().then((data) => {
     console.timeEnd('loc2')
+  })
+}, 250)
+
+setTimeout(() => { // prevent f4f server from blocking the request
+  console.time('loc_textgen')
+  getLocationsTextgen().then((data) => {
+    console.timeEnd('loc_textgen')
   })
 }, 500)
 
